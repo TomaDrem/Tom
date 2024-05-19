f5693ef836fd7fad9d910949138471da04708d37
Toma Drem <tvdrem4@gmail.com>
2024-05-19 09:14:49 +0300

    Markers

--- tests/api/test_api.py
+++ tests/api/test_api.py
@@ -1,3 +1,20 @@
+import pytest
+
+
+@pytest.mark.change
+def test_remove_name(user):
+    user.name = ''
+    assert user.name == ''
+
+
+@pytest.mark.check
+def test_name(user):
+    assert user.name == 'Viktor'
+
+
+@pytest.mark.check
+def test_second_name(user):
+    assert user.second_name == 'Sokol'
 
 
 
