35b276574c13c695fd8c4f1b3731338249587e46
Toma Drem <tvdrem4@gmail.com>
2024-05-19 19:23:14 +0300

    Correction of markers

--- tests/api/test_api.py
+++ tests/api/test_api.py
@@ -18,14 +18,14 @@ def test_second_name(user):
 
 
 
-def test_remove_name(user):
-    user.name = ''
-    assert user.name == ''
+# def test_remove_name(user):
+#     user.name = ''
+#     assert user.name == ''
 
 
-def test_name(user):
-    assert user.name == "Viktor"
+# def test_name(user):
+#     assert user.name == "Viktor"
 
 
-def test_second_name(user):
-    assert user.second_name == "Sokol"
+# def test_second_name(user):
+#     assert user.second_name == "Sokol"
--- tests/api/test_fixture.py
+++ tests/api/test_fixture.py
@@ -10,9 +10,9 @@ def test_change_second_name(user):
     assert user.second_name == 'Sokol'
 
 
-def test_change_name(user):
-    assert user.name == 'Viktor'
+# def test_change_name(user):
+#     assert user.name == 'Viktor'
 
 
-def test_change_second_name(user):
-    assert user.second_name == 'Sokol'
+# def test_change_second_name(user):
+#     assert user.second_name == 'Sokol'
